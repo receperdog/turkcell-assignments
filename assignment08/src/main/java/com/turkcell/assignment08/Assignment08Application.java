72b73848be966b900a18c21d0c8a9f0fd62cc96d
Recep Tayyip Erdogan <receptayyiperdogan8@gmail.com>
2022-11-28 00:56:29 +0300

    Spring boot app created & JPA rep used

---
+++ assignment08/src/main/java/com/turkcell/assignment08/Assignment08Application.java
@@ -0,0 +1,13 @@
+package com.turkcell.assignment08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment08Application.class, args);
+    }
+
+}
